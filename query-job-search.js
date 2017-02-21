9178f99addf36f796be7775b5e9aee4135a49191
brownman <brownman556@gmail.com>
2017-02-21 15:15:06 +0200

    Update query-job-search.js

--- query-job-search.js
+++ query-job-search.js
@@ -1,2 +1,3 @@
 - find HR ?
 https://www.linkedin.com/search/results/people/?facetIndustry=%5B%22137%22%5D&keywords=Jobnet&origin=FACETED_SEARCH
+https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%221215916%22%5D&facetGeoRegion=%5B%22il%3A0%22%5D&facetIndustry=%5B%22137%22%5D&keywords=Jobnet&origin=FACETED_SEARCH
