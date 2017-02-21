25fffe1a4fa3c52c8641fa1e7183e6c81cd1be0c
brownman <brownman556@gmail.com>
2017-02-21 15:23:27 +0200

    Update query-job-search.js

--- query-job-search.js
+++ query-job-search.js
@@ -1,3 +1,6 @@
-- find HR ?
+- find HR person ?
 https://www.linkedin.com/search/results/people/?facetIndustry=%5B%22137%22%5D&keywords=Jobnet&origin=FACETED_SEARCH
 https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%221215916%22%5D&facetGeoRegion=%5B%22il%3A0%22%5D&facetIndustry=%5B%22137%22%5D&keywords=Jobnet&origin=FACETED_SEARCH
+
+- find post with keyword (=location)
+- https://www.linkedin.com/search/results/content/?keywords=%D7%A8%D7%97%D7%95%D7%91%D7%95%D7%AA&origin=GLOBAL_SEARCH_HEADER
