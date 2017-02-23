f80c76c4a4f27220cc7050e8e885f00856270c30
brownman <brownman556@gmail.com>
2017-02-23 09:36:00 +0200

    Update query-job-search.js

--- query-job-search.js
+++ query-job-search.js
@@ -15,3 +15,9 @@ https://www.linkedin.com/search/results/people/?facetCurrentCompany=%5B%22121591
 
 - pin job location not-on-map (KM)
 - https://il.indeed.com/jobs?q=javascript&rs=1&l=%D7%A8%D7%97%D7%95%D7%91%D7%95%D7%AA%2C+%D7%9E%D7%97%D7%95%D7%96+%D7%94%D7%9E%D7%A8%D7%9B%D7%96&radius=10
+
+
+- information security + israel
+- query: cyber
+- filter: israel
+- https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22il%3A0%22%5D&keywords=cyber&origin=FACETED_SEARCH&page=2
